c254f9d478dca308efeb2817d7d253dfa76544b8
Mustaqeem Raza <77394916+mustaqeem-raza@users.noreply.github.com>
2024-12-10 01:46:56 +0500

    Breeze Auth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,101 +1,47 @@
-<!DOCTYPE html>
-<html lang="en">
+<x-guest-layout>
+    <!-- Session Status -->
+    <x-auth-session-status class="mb-4" :status="session('status')" />
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>NobleUI Responsive Bootstrap 4 Dashboard Template</title>
-    <!-- core:css -->
-    <link rel="stylesheet" href="{{asset('assets/vendors/core/core.css')}}">
-    <!-- endinject -->
-    <!-- plugin css for this page -->
-    <!-- end plugin css for this page -->
-    <!-- inject:css -->
-    <link rel="stylesheet" href="{{asset('assets/fonts/feather-font/css/iconfont.css')}}">
-    <link rel="stylesheet" href="{{asset('assets/vendors/flag-icon-css/css/flag-icon.min.css')}}">
-    <!-- endinject -->
-    <!-- Layout styles -->
-    <link rel="stylesheet" href="{{asset('assets/css/demo_1/style.css')}}">
-    <!-- End layout styles -->
-    <link rel="shortcut icon" href="{{asset('assets/images/favicon.png')}}" />
-</head>
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
 
-<body class="sidebar-dark">
-    <div class="main-wrapper">
-        <div class="page-wrapper full-page">
-            <div class="page-content d-flex align-items-center justify-content-center">
-
-                <div class="row w-100 mx-0 auth-page">
-                    <div class="col-md-8 col-xl-6 mx-auto">
-                        <div class="card">
-                            <div class="row">
-                                <div class="col-md-4 pr-md-0">
-                                    <div class="auth-left-wrapper">
-
-                                    </div>
-                                </div>
-                                <div class="col-md-8 pl-md-0">
-                                    <div class="auth-form-wrapper px-4 py-5">
-                                        <a href="#" class="noble-ui-logo d-block mb-2">Sumar <span>Well</span></a>
-                                        <h5 class="text-muted font-weight-normal mb-4">Welcome back! Log in to your account.</h5>
-                                        <form class="forms-sample" method="POST" action="{{ route('login') }}">
-                                            @csrf
-                                            <div class="form-group">
-                                                <label for="exampleInputEmail1">Email address</label>
-                                                <input
-                                                    type="email"
-                                                    class="form-control"
-                                                    id="exampleInputEmail1"
-                                                    placeholder="Email"
-                                                    autocomplete="email"
-                                                    name="email"
-                                                    value="{{ old('email') }}"
-                                                    required
-                                                    autofocus>
-                                                <x-input-error :messages="$errors->get('email')" class="mt-2" />
-                                            </div>
-
-                                            <div class="form-group">
-                                                <label for="exampleInputPassword1">Password</label>
-                                                <input
-                                                    type="password"
-                                                    class="form-control"
-                                                    id="exampleInputPassword1"
-                                                    autocomplete="current-password"
-                                                    placeholder="Password"
-                                                    name="password"
-                                                    required>
-                                                <x-input-error :messages="$errors->get('password')" class="mt-2" />
-                                            </div>
-
-                                            <div class="mt-3">
-                                                <button type="submit" class="btn btn-primary mr-2 mb-2 mb-md-0 text-white">Login</button>
-                                            </div>
-                                            <a href="#" class="d-block mt-3 text-muted">Forgot password?</a>
-                                        </form>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
+        <!-- Email Address -->
+        <div>
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
-    </div>
 
-    <!-- core:js -->
-    <script src="{{asset('assets/vendors/core/core.js')}}"></script>
-    <!-- endinject -->
-    <!-- plugin js for this page -->
-    <!-- end plugin js for this page -->
-    <!-- inject:js -->
-    <script src="{{asset('assets/vendors/feather-icons/feather.min.js')}}"></script>
-    <script src="{{asset('assets/js/template.js')}}"></script>
-    <!-- endinject -->
-    <!-- custom js for this page -->
-    <!-- end custom js for this page -->
-</body>
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Password')" />
 
-</html>
+            <x-text-input id="password" class="block mt-1 w-full"
+                            type="password"
+                            name="password"
+                            required autocomplete="current-password" />
+
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+
+        <!-- Remember Me -->
+        <div class="block mt-4">
+            <label for="remember_me" class="inline-flex items-center">
+                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+            </label>
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            @if (Route::has('password.request'))
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                    {{ __('Forgot your password?') }}
+                </a>
+            @endif
+
+            <x-primary-button class="ms-3">
+                {{ __('Log in') }}
+            </x-primary-button>
+        </div>
+    </form>
+</x-guest-layout>
--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,77 +1,29 @@
-@extends('layouts.app')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Profile') }}
+        </h2>
+    </x-slot>
 
-@section('content')
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
+                    @include('profile.partials.update-profile-information-form')
+                </div>
+            </div>
 
-<div class="page-content">
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
+                    @include('profile.partials.update-password-form')
+                </div>
+            </div>
 
-    <nav class="page-breadcrumb">
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="#">Forms</a></li>
-            <li class="breadcrumb-item active" aria-current="page">Basic Elements</li>
-        </ol>
-    </nav>
-
-    <div class="row">
-        <div class="col-md-12 stretch-card">
-            <div class="card">
-                <div class="card-body">
-                    <h6 class="card-title">Form Grid</h6>
-                    <form>
-                        <div class="row">
-                            <div class="col-sm-6">
-                                <div class="form-group">
-                                    <label class="control-label">First Name</label>
-                                    <input type="text" class="form-control" placeholder="Enter first name">
-                                </div>
-                            </div><!-- Col -->
-                            <div class="col-sm-6">
-                                <div class="form-group">
-                                    <label class="control-label">Last Name</label>
-                                    <input type="text" class="form-control" placeholder="Enter last name">
-                                </div>
-                            </div><!-- Col -->
-                        </div><!-- Row -->
-                        <div class="row">
-                            <div class="col-sm-4">
-                                <div class="form-group">
-                                    <label class="control-label">City</label>
-                                    <input type="text" class="form-control" placeholder="Enter city">
-                                </div>
-                            </div><!-- Col -->
-                            <div class="col-sm-4">
-                                <div class="form-group">
-                                    <label class="control-label">State</label>
-                                    <input type="text" class="form-control" placeholder="Enter state">
-                                </div>
-                            </div><!-- Col -->
-                            <div class="col-sm-4">
-                                <div class="form-group">
-                                    <label class="control-label">Zip</label>
-                                    <input type="text" class="form-control" placeholder="Enter zip code">
-                                </div>
-                            </div><!-- Col -->
-                        </div><!-- Row -->
-                        <div class="row">
-                            <div class="col-sm-6">
-                                <div class="form-group">
-                                    <label class="control-label">Email address</label>
-                                    <input type="email" class="form-control" placeholder="Enter email">
-                                </div>
-                            </div><!-- Col -->
-                            <div class="col-sm-6">
-                                <div class="form-group">
-                                    <label class="control-label">Password</label>
-                                    <input type="password" class="form-control" autocomplete="off"
-                                        placeholder="Password">
-                                </div>
-                            </div><!-- Col -->
-                        </div><!-- Row -->
-                    </form>
-                    <button type="button" class="btn btn-primary submit">Submit form</button>
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
+                    @include('profile.partials.delete-user-form')
                 </div>
             </div>
         </div>
     </div>
-
-</div>
-@endsection
+</x-app-layout>
